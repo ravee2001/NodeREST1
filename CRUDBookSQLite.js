27974edf6c4340ca4779f8c092d0cde4528cac55
Sirinya <s6406021620076@email.kmutnb.ac.th>
2023-02-15 11:34:24 +0700

    update

--- CRUDBookSQLite.js
+++
@@ -1,9 +0,0 @@
-// SQlite3 CRUD operations
-// npm install sqlite3
-//Create a Book.sqlite file in Database folder
-// Run this file with node CRUDBookSQLite.js
-// Test with Postamn
-
-const express = require('express');
-const sqlite3 = require('sqlite3');
-const app = express();
