0b83c75784a9a27c39a78216d53774d388b017cd
Sirinya <s6406021620076@email.kmutnb.ac.th>
2023-02-15 11:49:53 +0700

    change index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Go to The hell!");
+  res.send("asdfasdfasdfasdfasdfasdfasdfasdfadsfasdfas");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
